2084535fbe68564f73cc1433aa57347a89fc0356
Rubén Molina <4993rmp2901@gmail.com>
2021-09-24 11:22:36 +0200

    Ejercicio2a
    Contador que para cuando llega a la condición dada.

---
+++ Practica1Programacion/Assets/Scripts/Bucles.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucles : MonoBehaviour
+{
+
+    public int num1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(num1 < 10)
+        {
+            num1++;
+        }
+    }
+}
